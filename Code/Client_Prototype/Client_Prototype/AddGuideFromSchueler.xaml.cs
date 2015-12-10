19e2fe011ad3fe59bc609b66cab19173836aa804
Simsi4 <simon.schwantler@gmx.at>
2015-12-10 11:34:34 +0100

    Kommunikation mit Server für Guides und Schüler in 2 GUIs

--- Code/Client_Prototype/Client_Prototype/AddGuideFromSchueler.xaml.cs
+++ Code/Client_Prototype/Client_Prototype/AddGuideFromSchueler.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,48 +24,43 @@ namespace Client_Prototype
     public partial class AddGuideFromSchueler : Window
     {
         Window myParent;
+        private BackgroundWorker bw_Schueler = new BackgroundWorker();
+        private BackgroundWorker bw_Guides = new BackgroundWorker();
 
         public AddGuideFromSchueler()
         {
             InitializeComponent();
-            fillListGuides();
-            fillComboSchueler();
+            fillData();
         }
 
         public AddGuideFromSchueler(Window _parent)
         {
             InitializeComponent();
             myParent = _parent;
-            fillListGuides();
-            fillComboSchueler();
+            fillData();
         }
 
-        private void fillComboSchueler()
+        private void fillData()
         {
-            //TODO
-            //Get Schueler where isGuide = false
-            List<Schueler> content = new List<Schueler>();
-            content.Add(new Schueler(1, "Hansi", "Jaeger", "5BHIFS", false));
-            content.Add(new Schueler(1, "Markus", "Weber", "5BHIFS", false));
-            content.Add(new Schueler(1, "Michael", "Delfser", "5BHIFS", false));
-            cmbSchueler.ItemsSource = content;
+            bw_Schueler.WorkerReportsProgress = false;
+            bw_Schueler.WorkerSupportsCancellation = false;
+            bw_Schueler.DoWork += new DoWorkEventHandler(bw_DoWorkSchueler);
+            bw_Schueler.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompletedSchueler);
+            bw_Schueler.RunWorkerAsync();
+
+
+            bw_Guides.WorkerReportsProgress = false;
+            bw_Guides.WorkerSupportsCancellation = false;
+            bw_Guides.DoWork += new DoWorkEventHandler(bw_DoWorkGuide);
+            bw_Guides.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompletedGuide);
+            bw_Guides.RunWorkerAsync();
         }
 
-        private void fillListGuides()
-        {
-            //TODO
-            //Get Schueler where isGuide = true
-            List<Schueler> content = new List<Schueler>();
-            content.Add(new Schueler(1, "Jonas", "Schaltegger", "5BHIFS", true));
-            content.Add(new Schueler(2, "Simon", "Schwantler", "5BHIFS", true));
-            content.Add(new Schueler(3, "Henrik", "Csoere", "5BHIFS", true));
-            content.Add(new Schueler(4, "Richard", "Neumann", "5AHIFS", true));
-            content.Add(new Schueler(5, "Sandro", "Linder", "4AHIFS", true));
-            gridGuides.ItemsSource = content;
-        }
+
 
         private void btnAddGuide_Click(object sender, RoutedEventArgs e)
         {
+            /*
             Schueler toAdd = ((Schueler)cmbSchueler.SelectedItem);
             toAdd.S_isGuide = true;
             //TODO
@@ -69,12 +68,65 @@ namespace Client_Prototype
             //call fillComboSchueler
             //call fillListGuides
             lblMessage.Content = "Guide Added";
+            */
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
             this.Close();
             myParent.Show();
+        }
+
+        private void bw_DoWorkSchueler(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            HttpWebRequest req = WebRequest.Create(new Uri("http://192.168.196.0:8080/TatueOrganiser/api/schueler")) as HttpWebRequest;
+            req.Method = "GET";
+
+            req.ContentType = "application/json";
+            req.Accept = "application/json";
+            using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(resp.GetResponseStream());
+                e.Result = reader.ReadToEnd();
+            }
+        }
+
+        private void bw_RunWorkerCompletedSchueler(object sender, RunWorkerCompletedEventArgs e)
+        {
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            Schueler[] schueler = (Schueler[])json_serializer.Deserialize<Schueler[]>((String)e.Result);
+            List<Schueler> content = new List<Schueler>(schueler);
+            Console.WriteLine((String)e.Result);
+            Console.WriteLine(content[0].ToString());
+            cmbSchueler.ItemsSource = schueler;
+        }
+
+        private void bw_DoWorkGuide(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            HttpWebRequest req = WebRequest.Create(new Uri("http://192.168.196.0:8080/TatueOrganiser/api/guides")) as HttpWebRequest;
+            req.Method = "GET";
+
+            req.ContentType = "application/json";
+            req.Accept = "application/json";
+            using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(resp.GetResponseStream());
+                e.Result = reader.ReadToEnd();
+            }
+        }
+
+        private void bw_RunWorkerCompletedGuide(object sender, RunWorkerCompletedEventArgs e)
+        {
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            Schueler[] guides = (Schueler[])json_serializer.Deserialize<Schueler[]>((String)e.Result);
+            List<Schueler> content = new List<Schueler>(guides);
+            Console.WriteLine((String)e.Result);
+            Console.WriteLine(content[0].ToString());
+            gridGuides.ItemsSource = guides;
         }
     }
 }
--- Code/Client_Prototype/Client_Prototype/Classes/Schueler.cs
+++ Code/Client_Prototype/Client_Prototype/Classes/Schueler.cs
@@ -6,19 +6,24 @@ namespace Client_Prototype
     public class Schueler
     {
         public int S_ID { get; set; }
-        public String S_Vorname { get; set; }
-        public String S_Nachname { get; set; }
-        public String S_Klasse { get; set; }
-        public Boolean S_isGuide { get; set; }
+        public String Vorname { get; set; }
+        public String Nachname { get; set; }
+        public String Klasse { get; set; }
+        public int isGuide { get; set; }
         public List<GuideRating> S_allRatings = new List<GuideRating>();
 
-        public Schueler(int _ID, String _Vorname, String _Nachname, String _Klasse, Boolean _isGuide)
+        public Schueler()
+        {
+
+        }
+
+        public Schueler(int _ID, String _Vorname, String _Nachname, String _Klasse, int _isGuide)
         {
             S_ID = _ID;
-            S_Vorname = _Vorname;
-            S_Nachname = _Nachname;
-            S_Klasse = _Klasse;
-            S_isGuide = _isGuide;
+            Vorname = _Vorname;
+            Nachname = _Nachname;
+            Klasse = _Klasse;
+            isGuide = _isGuide;
         }
 
         public void addRatingToSchueler(GuideRating _Rating)
@@ -33,7 +38,7 @@ namespace Client_Prototype
 
         public override String ToString()
         {
-            return this.S_Nachname + " " + S_Vorname + ", " + S_Klasse;
+            return this.Nachname + " " + this.Vorname + ", " + this.Klasse;
         }
 
         public void resetRatings()
